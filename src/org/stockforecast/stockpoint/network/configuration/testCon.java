7916892ce5405a9de726bb6f0b5ab77e3833a582
rogetfan <huxuehan1991@gmail.com>
2014-04-23 21:34:22 +0800

    no thing to say

---
+++ src/org/stockforecast/stockpoint/network/configuration/testCon.java
@@ -0,0 +1,12 @@
+package org.stockforecast.stockpoint.network.configuration;
+
+import java.io.IOException;
+
+public class testCon {
+
+	public static void main(String[] args) throws IOException {
+		SetConfiguration SC= new SetConfiguration();
+        SC.set();
+	}
+
+}
